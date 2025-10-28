3c459a00a2c0908d2963517beaa599de9646eba2
reysilvaa <reynaldsilva123@gmail.com>
2025-10-29 04:26:23 +0700

    Update Netlify configuration to use '@sveltejs/adapter-netlify', modify headers for caching and content types, and adjust package dependencies for improved build process.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,11 +11,8 @@ const config = {
   
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
+      edge: false,
+      split: false
     }),
     alias: {
       '$lib': 'src/lib'
